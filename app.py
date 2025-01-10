81b49c0c9a3b75c09d797cc26f4d4d80d5c87d54
tonino-stuta <119683972+tonino-stuta@users.noreply.github.com>
2025-01-10 12:29:10 +0000

    1

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("index.html")
+
+@app.route("/signin")
+def signin():
+    return render_template("signin.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 
 app.run()
